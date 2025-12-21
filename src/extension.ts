3142407fcb9f787ad39c1d72591d42f1812889a9
Christian Konnaris <christian@MacBookPro.telekom.ip>
2025-12-21 11:29:17 +0100

    Update package dependencies and extension metadata
    Add prettier to dev dependencies
    
    Update command implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,38 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { commands, ExtensionContext, window } from 'vscode';
+import { commands, ExtensionContext, window } from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "console-log-remover" is now active! 🎉',
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "console-log-remover" is now active! 🎉');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const removeCommand = commands.registerCommand(
+    "console-log-remover.remove",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      window.showInformationMessage("Hello World from Console.log Remover!");
+    },
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = commands.registerCommand('console-log-remover.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		window.showInformationMessage('Hello World from Console.log Remover!');
-	});
+  const toggleCommand = commands.registerCommand(
+    "console-log-remover.toggle",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      window.showInformationMessage("Hello World from Console.log Remover!");
+    },
+  );
 
-
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(removeCommand, toggleCommand);
 }
 
 // This method is called when your extension is deactivated
