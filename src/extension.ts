ea4aace561d525eeb23311a018edf04d86caff01
Christian Konnaris <christian@MacBookPro.telekom.ip>
2025-12-22 18:29:39 +0100

    Remove unused VS Code import

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as parser from "@babel/parser";
 import traverse from "@babel/traverse";
 import * as t from "@babel/types";
